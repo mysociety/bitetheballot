b629e720bf64482a965efb8ef1b66bff2b70eaf9
Steven Day <steve@mysociety.org>
2015-05-05 11:39:00 +0100

    Tidy up vote match calculations

--- calculate_vote_match.py
+++ calculate_vote_match.py
@@ -1,12 +1,16 @@
 import json
 import argparse
 from collections import defaultdict
+import logging
 
 import unicodecsv
 from progressbar import ProgressBar
 
 import utils
-from constants import PARTY_POSITIONS
+from constants import PARTY_POSITIONS, NUM_STATEMENTS, VOTE_MATCH_FIELDS
+
+
+log = logging.getLogger(__name__)
 
 
 def statements_by_topic(party_positions):
@@ -85,33 +89,33 @@ def user_weighted_party_matches_by_party_topic_statement(decisions, user_parties
 
         user_weight = int(decision['weight'])  # Either 1 or 2
 
-        print combined_id
-        print "user position is: {0}".format(user_position)
-        print "user weight is: {0}".format(user_weight)
-        print "Comparing user position to parties"
+        log.debug(combined_id)
+        log.debug("user position is: {0}".format(user_position))
+        log.debug("user weight is: {0}".format(user_weight))
+        log.debug("Comparing user position to parties")
         for party in user_parties:
-            print "party {0}".format(party)
+            log.debug("party {0}".format(party))
             party_position = party_decisions_by_statement[combined_id][party]
-            print "party position is {0}".format(party_position)
+            log.debug("party position is {0}".format(party_position))
             party_match = 0
             if user_position == party_position:
                 party_match = 1
-            print "party match is {0}".format(party_match)
+            log.debug("party match is {0}".format(party_match))
             party_weighted_match = party_match * user_weight
-            print "weighted match is {0}".format(party_weighted_match)
+            log.debug("weighted match is {0}".format(party_weighted_match))
             weighted_party_matches[party][topic_id][statement_id] = party_weighted_match
     return weighted_party_matches
 
 
 def user_total_weighted_party_matches_by_topic(weighted_party_matches_by_topic):
     total_party_matches = defaultdict(dict)
-    print "Calculating total matches by topic"
+    log.debug("Calculating total matches by topic")
     for party, topics in weighted_party_matches_by_topic.iteritems():
-        print "Calculating total matches by topic for party: {0}".format(party)
-        print "Matches per topic are: {0}".format(topics)
+        log.debug("Calculating total matches by topic for party: {0}".format(party))
+        log.debug("Matches per topic are: {0}".format(topics))
         for topic, statements in topics.iteritems():
-            print "statements for {0} are: {1}".format(topic, statements)
-            print "sum of values is: {0}".format(sum(statements.itervalues()))
+            log.debug("statements for {0} are: {1}".format(topic, statements))
+            log.debug("sum of values is: {0}".format(sum(statements.itervalues())))
             total_party_matches[topic][party] = sum(statements.itervalues())
     return total_party_matches
 
@@ -119,37 +123,37 @@ def user_total_weighted_party_matches_by_topic(weighted_party_matches_by_topic):
 def user_percentage_party_matches_by_topic(total_party_matches_by_topic):
     percentage_party_matches = defaultdict(dict)
     for topic, parties in total_party_matches_by_topic.iteritems():
-        print "totals for topic: {0} are {1}".format(topic, parties)
+        log.debug("totals for topic: {0} are {1}".format(topic, parties))
         total_party_matches_sum = sum(parties.itervalues())
-        print "Sum of all party matches: {0}".format(total_party_matches_sum)
+        log.debug("Sum of all party matches: {0}".format(total_party_matches_sum))
         # Then we work out what percentage each party is of that total
         for party, total_party_match in parties.iteritems():
-            print "Total party match for {0} is {1}".format(party, total_party_match)
+            log.debug("Total party match for {0} is {1}".format(party, total_party_match))
             percentage_party_match = (float(total_party_match) / float(total_party_matches_sum)) * 100
-            print "Percentage party match for {0} is {1}".format(party, percentage_party_match)
+            log.debug("Percentage party match for {0} is {1}".format(party, percentage_party_match))
             percentage_party_matches[topic][party] = percentage_party_match
     return percentage_party_matches
 
 
-def user_percentage_party_matches(user_parties, num_topics, total_party_matches_by_topic):
+def user_percentage_party_matches(user_parties, num_statements, total_party_matches_by_topic):
     """
     Calculate the percentage by which the user matches the party overall, from
     all of their individual answers.
     """
     percentage_party_matches = {}
-    print total_party_matches_by_topic
-    print "total topics are: {0}".format(num_topics)
+    log.debug(total_party_matches_by_topic)
+    log.debug("total topics are: {0}".format(num_statements))
     for party in user_parties:
-        print "Calcuating percentage match for {0}".format(party)
+        log.debug("Calcuating percentage match for {0}".format(party))
         # Calculate the total match for this party over all the topics
         sum_total_matches = 0
         for topic, parties in total_party_matches_by_topic.iteritems():
-            print "on topic {0} - total matches are: {1}".format(topic, parties)
-            print "Total sum for this party is: {0}".format(parties[party])
+            log.debug("on topic {0} - total matches are: {1}".format(topic, parties))
+            log.debug("Total sum for this party is: {0}".format(parties[party]))
             sum_total_matches += parties[party]
-        print "Sum of all matches is {0}".format(sum_total_matches)
-        percentage_party_matches[party] = (float(sum_total_matches) / float(num_topics)) * 100
-        print "Percentage match is: {0}".format(percentage_party_matches[party])
+        log.debug("Sum of all matches is {0}".format(sum_total_matches))
+        percentage_party_matches[party] = (float(sum_total_matches) / float(num_statements)) * 100
+        log.debug("Percentage match is: {0}".format(percentage_party_matches[party]))
     return percentage_party_matches
 
 
@@ -169,12 +173,16 @@ if __name__ == '__main__':
         type=argparse.FileType('r'),
         help="Input bitetheballot.json users file"
     )
-    parser.add_argument(
-        'output_file',
-        type=str,
-        help="Output CSV file"
-    )
+    parser.add_argument('output_file', type=str, help="Output CSV file")
+    parser.add_argument('--verbosity', type=int)
     args = parser.parse_args()
+
+    if args.verbosity == 3:
+        log.setLevel(logging.DEBUG)
+    elif args.verbosity == 2:
+        log.setLevel(logging.INFO)
+    else:
+        log.setLevel(logging.ERROR)
 
     # These were parsed from the original json file but are hard-coded here
     # for the sake of simplicity.
@@ -193,14 +201,13 @@ if __name__ == '__main__':
     # Build a list of all the statements by topic. We use the PARTY_POSITIONS
     # dict for this because its keys are a handy list of them all.
     STATEMENTS_BY_TOPIC = statements_by_topic(PARTY_POSITIONS)
-    NUM_TOPICS = len(PARTY_POSITIONS.keys())
 
     rows = []
     users = json.load(args.users_input_file)
     # Loop over every user
-    print "Processing {0} users".format(len(users))
+    log.info("Processing {0} users".format(len(users)))
     progress_bar = ProgressBar(len(users))
-    #progress_bar.start()
+    progress_bar.start()
 
     for user_id, user in users.items():
         # Instantiate the row
@@ -219,40 +226,33 @@ if __name__ == '__main__':
                 user_parties,
                 PARTY_POSITIONS
             )
-            print "Weighted party matches by topic are: {0}".format(weighted_party_matches_by_topic)
+            log.debug("Weighted party matches by topic are: {0}".format(weighted_party_matches_by_topic))
             # Total those up by topic
             total_party_matches_by_topic = user_total_weighted_party_matches_by_topic(weighted_party_matches_by_topic)
             # Then work out percentages by topic
-            print "Total party matches are: {0}".format(total_party_matches_by_topic)
+            log.debug("Total party matches are: {0}".format(total_party_matches_by_topic))
             percentage_party_matches_by_topic = user_percentage_party_matches_by_topic(total_party_matches_by_topic)
-            print "Percentage party matches are: {0}".format(percentage_party_matches_by_topic)
+            log.debug("Percentage party matches are: {0}".format(percentage_party_matches_by_topic))
             # Then calculate the overall
             overall_party_matches = user_percentage_party_matches(
                 user_parties,
-                NUM_TOPICS,
+                NUM_STATEMENTS,
                 total_party_matches_by_topic
             )
-            print "Overall party matches are: {0}".format(overall_party_matches)
+            log.debug("Overall party matches are: {0}".format(overall_party_matches))
 
             # Created flattened dictionaries to save to the CSV row
             flattened_percentage_party_matches_by_topic = flatten_percentage_party_matches_by_topic(percentage_party_matches_by_topic)
             row.update(flattened_percentage_party_matches_by_topic)
             row.update(overall_party_matches)
             rows.append(row)
-            #progress_bar.update(1)
+            progress_bar.update(1)
 
-    #progress_bar.finish()
+    progress_bar.finish()
 
     # Write everything out to the CSV file - this will add a lot of columns!
-    topic_party_match_columns = []
-    for topic in STATEMENTS_BY_TOPIC:
-        for party in ALL_PARTIES:
-            column_name = "{0}_{1}".format(topic, party)
-            topic_party_match_columns.append(column_name)
-
-    fields = ['user_id'] + list(topic_party_match_columns) + list(ALL_PARTIES)
-
+    output_fields = ['user_id'] + list(VOTE_MATCH_FIELDS)
     with open(args.output_file, 'wb') as f:
-        writer = unicodecsv.DictWriter(f, fields)
+        writer = unicodecsv.DictWriter(f, output_fields)
         writer.writeheader()
         writer.writerows(rows)
